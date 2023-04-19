549053d36c0e39b6ff2f4f8e59748cad3a1197ec
Johnnywiki <Johnnywiki73@gmail.com>
2023-04-19 12:43:29 -0300

    Create tipoDeAlimentação.java
    Criado método de retorno básico sobre o alimento inserido com texto. Foi adicionado também a variável boolean caso seja decidido usa-lá

---
+++ tipoDeAlimentação.java
@@ -0,0 +1,22 @@
+import java.util.RandomAccess;
+
+public class String tipoDeAlimentacao(String alimento){
+private boolean racao equals false;
+private boolean soro equals false;
+private boolean outro equals false;
+
+if(alimento == null) { return System.out.println("Alimento não foi inserido!");
+    else if(alimento.equals("racao")){
+        racao equals true;
+        return System.out.println("A alimentação do animal consiste de " + alimento);
+        else if(alimento.equals("soro"){
+            soro equals true;
+            return System.out.println("A alimentação do animal consiste de " + alimento);
+            else(alimento.equals("outro")){
+                outro equals true;
+                System.out.println("A alimentação do animal consiste de " + alimento);
+            }
+        }
+    }
+
+}}
