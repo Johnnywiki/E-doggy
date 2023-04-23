081e8f3a5762323a69d856c2e7bbca8664af1434
Johnnywiki <92699088+Johnnywiki@users.noreply.github.com>
2023-04-23 20:23:22 -0300

    Update cadastroDogGeneric.java

--- cadastroDogGeneric.java
+++ cadastroDogGeneric.java
@@ -1,14 +1,11 @@
 public class cadastroDog(class Cachorro){
     private arrayList<C> cadastro;
-}
-
-public class cadastroDog(){
     cadastro = new ArrayList<C>();
-}
-
-cadastroDog<C> cadastro = new cadastroDog<C>();
-cadastro.add();
-
-for(int i=0,i < cadastro.size(), i++){
-    System.out.println(cadastro.get(i));
+    cadastroDog<C> cadastro = new cadastroDog<C>();
+    cadastro.add();
+    
+    public void cadastroReturn(){
+    for(int i=0,i < cadastro.size(), i++){
+        System.out.println(cadastro.get(i));
+    }
 }
