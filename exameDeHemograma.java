928fdbe504b51e88866d59b3d4e9eed6f551770a
Johnnywiki <92699088+Johnnywiki@users.noreply.github.com>
2023-04-23 19:54:22 -0300

    Update exameDeHemograma.java

--- exameDeHemograma.java
+++ exameDeHemograma.java
@@ -1,16 +1,16 @@
 public class hemograma extends exame(){
-    boolean sintomas equals false;
-    boolean abnormalidade equals false;
+    private boolean sintomas equals false;
+    private boolean abnormalidade equals false;
 
     System.out.println("O cachorro tem abnormalidades? Responda com: "S/N"");
     Scanner scanner = new Scanner(System.in);
     String scannerInput1 = scanner.nextline();
-    if(scannerInput1.equals("S")||("s")) abnormalidade equals true; else abnormalidade equals false;
+    if(scannerInput1.equals("S") or ("s")) abnormalidade equals true; else abnormalidade equals false;
 
     System.out.println("O cachorro tem sintomas? Responda com: "S/N"");
     Scanner scanner = new Scanner(System.in);
     String scannerInput2 = scanner.nextline();
-    if(scannerInput2.equals("S")||("s")) sintomas equals true; else sintomas equals false;
+    if(scannerInput2.equals("S") or ("s")) sintomas equals true; else sintomas equals false;
 
     if(sintomas||abnormalidade equals true) return System.out.println("Animal disposto a praticar exame!") else System.out.println("Animal indisposto a praticar exame!");
 
